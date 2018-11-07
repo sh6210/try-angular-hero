f1743740854ab120de5119675194bd85ec50b4b7
Sohag Hossain <sohag.hossain@sebpo.com>
2018-11-07 20:19:04 +0600

    deleted some dummy heroes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -6,9 +6,5 @@ export const HEROES: Hero[] = [
     {id: 13, name: 'Bombasto'},
     {id: 14, name: 'Celeritas'},
     {id: 15, name: 'Magneta'},
-    {id: 16, name: 'RubberMan'},
-    {id: 17, name: 'Dynama'},
-    {id: 18, name: 'Dr IQ'},
-    {id: 19, name: 'Magma'},
-    {id: 20, name: 'Tornado'}
+    {id: 16, name: 'RubberMan'}
 ];
